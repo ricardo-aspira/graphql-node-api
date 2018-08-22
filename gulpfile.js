b437bf6cf6fa92bd1ed344fd8d58386a948d584e
Ricardo Morais <ricardo.morais@ancine.gov.br>
2018-08-22 09:39:56 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// Código estilo Node.JS, não ES6
+const gulp  = require('gulp');
+const clean = require('gulp-clean');
+const ts    = require('gulp-typescript');
+
+// Cria a definição do projeto com base no arquivo de configurações do typescript
+const tsProject = ts.createProject('tsconfig.json');
+
+/*
+ * Definição da tarefa "scripts".
+ * Para iniciar, precisa que a tarefa "static" tenha sido concluída.
+ */ 
+gulp.task('scripts', ['static'], () => {
+    // Sobre o src, realiza o build
+    const tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    // Sobre o resultado do build, copiar o mesmo para o destino desejado
+    return tsResult.js
+        .pipe(gulp.dest('dist'));
+});
+
+/*
+ * Definição da tarefa "static".
+ * Para iniciar, precisa que a tarefa "clean" tenha sido concluída.
+ */
+gulp.task('static', ['clean'], () => {
+    // Sobre o fonte, copia todos os arquivos *.json para a pasta "dist"
+    return gulp
+        .src(["src/**/*.json"])
+        .pipe(gulp.dest('dist'));
+});
+
+/*
+ * Definição da tarefa "clean".
+ */
+gulp.task('clean', () => {
+    // Limpar a pasta "dist"
+    return gulp
+        .src('dist')
+        .pipe(clean());
+});
+
+/*
+ * Tarefa de build executará essas tarefas, porém
+ * a execução é em paralelo, para não termos erros,
+ * devemos colocar dependências entre tarefas.
+ */
+gulp.task('build', ['clean', 'static', 'scripts']);
+
+/*
+ * Definição da tarefa "watch".
+ * Para iniciar, precisa que a tarefa "build" tenha sido concluída.
+ */
+gulp.task('watch', ['build'], () => {
+    // Sempre que houver uma alteração nos arquivos "*.ts" e
+    // "*.json", o gulp executará a tarefa "build"
+    return gulp.watch(["src/**/*.ts", "src/**/*.json"], ['build']);
+});
+
+// Define que a tarefa default é a "watch".
+gulp.task('default', ['watch']);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+let a: string = 'Aspira Ricardo';
+
+console.log(a);
