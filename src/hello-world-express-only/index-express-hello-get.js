5b448b208f5f8c3fbfcab0bbbfcdf8a82444e1a7
Ricardo Morais <ricardo.morais@ancine.gov.br>
2018-08-28 13:38:43 -0300

    Remoção dos arquivos js

--- src/hello-world-express-only/index-express-hello-get.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Index de um servidor HTTP comum, escutando na porta 3000
-const http = require("http");
-const app_express_hello_get_1 = require("./app-express-hello-get");
-const server = http.createServer(app_express_hello_get_1.default);
-server.listen(3000);
-server.on('listening', () => console.log('Listening on port 3000...'));
